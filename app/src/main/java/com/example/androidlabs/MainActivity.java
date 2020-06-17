1c15510aea4985e13a5c1f9e649a43a8b692ede6
Jonathan Cordingley <cord0059@algonquinlive.com>
2020-06-16 21:34:13 -0400

    Finished lab 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,51 +2,51 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.Switch;
+import android.content.Intent;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String email;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.lab_3_layout);
 
-        TextView myTextView = findViewById(R.id.myText);
+        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        EditText emailEdit = findViewById(R.id.enterEmailEditText);
+        Button login = findViewById(R.id.loginButton);
 
-        Button btn = findViewById(R.id.clickHereButton);
-        btn.setOnClickListener(
-                (v)->
-                        Toast.makeText(this, this.getString(R.string.toast_message)  , Toast.LENGTH_LONG).show());
 
-        CheckBox cB = findViewById(R.id.checkBox);
-        cB.setOnCheckedChangeListener((view1, isChecked) ->{
-                    //                https://stackoverflow.com/questions/31090754/android-support-library-snackbar-with-indefinite-length
-                    if (isChecked) {
-                        Snackbar.make(cB, this.getString(R.string.snackOn), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.undo), view2 -> cB.setChecked(false)).show();
-                    } else{
-                        Snackbar.make(cB, this.getString(R.string.snackOff), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.undo), view2 -> cB.setChecked(true)).show();
-            }
+        login.setOnClickListener((v) -> {
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra("EMAIL", emailEdit.getText().toString());
+            startActivity(goToProfile);
+            saveSharedPrefs(emailEdit.getText().toString());
         });
+    }
 
-        Switch sw = findViewById(R.id.sw);
-        sw.setOnCheckedChangeListener((view1, isChecked) ->{
-            if (isChecked) {
-                Snackbar.make(sw, this.getString(R.string.snackOn), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.undo), view2 -> sw.setChecked(false)).show();
-            } else{
-                Snackbar.make(sw, this.getString(R.string.snackOff), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.undo), view2 -> sw.setChecked(true)).show();
-            }
-        });
 
-        ImageButton iButton = findViewById(R.id.imageButton);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        email = prefs.getString("ReserveName", "Default Value");
+        saveSharedPrefs(email);
 
+    }
+
+    private void saveSharedPrefs(String s) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("ReserveName", s);
+        editor.commit();
 
     }
 }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,82 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageButton mImageButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
+        setContentView(R.layout.activity_profile);
+
+        EditText emailEntry = findViewById(R.id.enterEmailEditText);
+        mImageButton = findViewById(R.id.pictureButton);
+
+        Intent fromMain = getIntent();
+        emailEntry.setText(fromMain.getStringExtra("EMAIL"));
+
+        mImageButton.setOnClickListener((v) -> {
+                    this.dispatchTakePictureIntent();
+                    this.onActivityResult(REQUEST_IMAGE_CAPTURE, RESULT_OK, fromMain);
+                }
+        );
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function: onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function: onDestroy");
+    }
+}
