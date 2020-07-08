3e28562c23eb4972e7649794533bb33da7c2911e
Jonathan Cordingley <cord0059@algonquinlive.com>
2020-07-08 16:24:27 -0400

    Finished Lab 5

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -3,9 +3,16 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -22,6 +29,133 @@ import android.widget.TextView;
 public class ChatRoomActivity extends AppCompatActivity {
     private ArrayList<Message> elements = new ArrayList<>();
     private MyListAdapter aListAdapter;
+    SQLiteDatabase db;
+    int positionClicked = 0;
+    private static int ACTIVITY_VIEW_MESSAGE = 33;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+
+        Button send = findViewById(R.id.sendButton);
+        Button receive = findViewById(R.id.receiveButton);
+        EditText typeMessage = findViewById(R.id.typeHere);
+
+
+        loadDataFromDatabase();
+
+        send.setOnClickListener(btn -> {
+                    String message = typeMessage.getText().toString();
+                    int isSend = 1;
+
+                    ContentValues newRowValues = new ContentValues();
+                    newRowValues.put(MyOpener.COL_MESSAGE, message);
+                    newRowValues.put(MyOpener.COL_SEND, isSend);
+
+                    long newId = db.insert(MyOpener.TABLE_NAME, null, newRowValues);
+
+                    Message sendMessage = new Message(typeMessage.getText().toString(), 1, newId);
+                    elements.add(sendMessage);
+                    aListAdapter.notifyDataSetChanged();
+                    typeMessage.getText().clear();
+                }
+        );
+
+        receive.setOnClickListener(btn -> {
+            String message = typeMessage.getText().toString();
+            int isSend = 0;
+
+            ContentValues newRowValues = new ContentValues();
+            newRowValues.put(MyOpener.COL_MESSAGE, message);
+            newRowValues.put(MyOpener.COL_SEND, isSend);
+
+            long newId = db.insert(MyOpener.TABLE_NAME, null, newRowValues);
+
+            Message sendMessage = new Message(typeMessage.getText().toString(), 0, newId);
+            elements.add(sendMessage);
+            aListAdapter.notifyDataSetChanged();
+            typeMessage.getText().clear();
+        });
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(aListAdapter = new MyListAdapter());
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            Message message = elements.get(position);
+            alertDialogBuilder.setTitle("Do you want to delete this?");
+
+            //What is the message:
+            alertDialogBuilder.setMessage("The selected row is: " + position + "\n\nThe database id is: " + id);
+
+            //What the yes button does:
+            alertDialogBuilder.setPositiveButton("Yes", (click, arg) -> {
+                elements.remove(position);
+                deleteMessage(message);
+                aListAdapter.notifyDataSetChanged();
+            });
+
+            //What the No button does:
+            alertDialogBuilder.setNegativeButton("No", (click, arg) -> {
+
+            });
+
+            //Show the dialog:
+            alertDialogBuilder.create().show();
+
+            return true;
+        });
+    }
+
+    private void loadDataFromDatabase() {
+        MyOpener dbOpener = new MyOpener(this);
+        db = dbOpener.getWritableDatabase(); //This calls onCreate() if you've never built the table before, or onUpgrade if the version here is newer
+
+
+        // We want to get all of the columns. Look at MyOpener.java for the definitions:
+        String[] columns = {MyOpener.COL_ID, MyOpener.COL_MESSAGE, MyOpener.COL_SEND};
+        //query all the results from the database:
+        Cursor results = db.query(false, MyOpener.TABLE_NAME, columns, null, null, null, null, null, null);
+
+        printCursor(results, db.getVersion());
+        //Now the results object has rows of results that match the query.
+        //find the column indices:
+        int messageColumnIndex = results.getColumnIndex(MyOpener.COL_MESSAGE);
+        int isSendColIndex = results.getColumnIndex(MyOpener.COL_SEND);
+        int idColIndex = results.getColumnIndex(MyOpener.COL_ID);
+
+        //iterate over the results, return true if there is a next item:
+        while (results.moveToNext()) {
+            int isSend = results.getInt(isSendColIndex);
+            String message = results.getString(messageColumnIndex);
+            long id = results.getLong(idColIndex);
+
+            //add the new Contact to the array list:
+            elements.add(new Message(message, isSend, id));
+        }
+
+        //At this point, the contactsList array has loaded every row from the cursor.
+    }
+
+    protected void updateMessage(Message message) {
+        //Create a ContentValues object to represent a database row:
+        ContentValues updatedValues = new ContentValues();
+        updatedValues.put(MyOpener.COL_SEND, message.isSend());
+        updatedValues.put(MyOpener.COL_MESSAGE, message.getTextMessage());
+
+        //now call the update function:
+        db.update(MyOpener.TABLE_NAME, updatedValues, MyOpener.COL_ID + "= ?", new String[]{Long.toString(message.getId())});
+    }
+
+    protected void deleteMessage(Message message) {
+        db.delete(MyOpener.TABLE_NAME, MyOpener.COL_ID + "= ?", new String[]{Long.toString(message.getId())});
+    }
+
+    protected void printCursor(Cursor c, int version) {
+        Log.v("Cursor Object", DatabaseUtils.dumpCursorToString(c));
+    }
+
 
     private class MyListAdapter extends BaseAdapter {
 
@@ -43,7 +177,7 @@ public class ChatRoomActivity extends AppCompatActivity {
             LayoutInflater inflater = getLayoutInflater();
             View newView;
 
-            if (msg.isSend()) {
+            if (msg.isSend() == 1) {
                 newView = inflater.inflate(R.layout.send_layout, parent, false);
             } else newView = inflater.inflate(R.layout.receive_layout, parent, false);
 
@@ -58,18 +192,24 @@ public class ChatRoomActivity extends AppCompatActivity {
 
     private class Message {
         private String textMessage;
-        private boolean isSend;
+        private int isSend;
+        private long id;
 
-        public Message(String textMessage, boolean isSend) {
+        public Message(String textMessage, int isSend, long id) {
             this.textMessage = textMessage;
             this.isSend = isSend;
+            this.id = id;
+        }
+
+        public Message(String textMessage, int isSend) {
+            this(textMessage, isSend, 0);
         }
 
         public String getTextMessage() {
             return textMessage;
         }
 
-        public boolean isSend() {
+        public int isSend() {
             return isSend;
         }
 
@@ -77,7 +217,7 @@ public class ChatRoomActivity extends AppCompatActivity {
             this.textMessage = textMessage;
         }
 
-        public void setSend(boolean send) {
+        public void setSend(int send) {
             isSend = send;
         }
 
@@ -85,53 +225,77 @@ public class ChatRoomActivity extends AppCompatActivity {
         public String toString() {
             return textMessage;
         }
+
+        public long getId() {
+            return id;
+        }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
+    public class MyOpener extends SQLiteOpenHelper {
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-        Button send = findViewById(R.id.sendButton);
-        EditText typeMessage = findViewById(R.id.typeHere);
-        Button receive = findViewById(R.id.receiveButton);
+        protected final static String DATABASE_NAME = "MessageDB";
+        protected final static int VERSION_NUM = 1;
+        public final static String TABLE_NAME = "MESSAGES";
+        public final static String COL_MESSAGE = "MESSAGE";
+        public final static String COL_SEND = "SEND";
+        public final static String COL_ID = "_id";
 
-        listView.setAdapter(aListAdapter = new MyListAdapter());
-        listView.setOnItemLongClickListener((parent, view, position, id) -> {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("Do you want to delete this?");
+        public MyOpener(Context ctx) {
+            super(ctx, DATABASE_NAME, null, VERSION_NUM);
+        }
 
-            //What is the message:
-            alertDialogBuilder.setMessage("The selected row is: " + position + "\n\nThe database id is: " + id);
 
-            //What the yes button does:
-            alertDialogBuilder.setPositiveButton("Yes", (click, arg) -> {
-                elements.remove(position);
-                aListAdapter.notifyDataSetChanged();
-            });
+        //This function gets called if no database file exists.
+        //Look on your device in the /data/data/package-name/database directory.
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COL_MESSAGE + " text,"
+                    + COL_SEND + " text);");  // add or remove columns
+        }
 
-            //What the No button does:
-            alertDialogBuilder.setNegativeButton("No", (click, arg) -> {
+        //this function gets called if the database version on your device is lower than VERSION_NUM
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {   //Drop the old table:
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
-            });
+            //Create the new table:
+            onCreate(db);
+        }
 
-            //Show the dialog:
-            alertDialogBuilder.create().show();
 
-        return true;
-        });
+        //this function gets called if the database version on your device is higher than VERSION_NUM
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {   //Drop the old table:
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
-        send.setOnClickListener((v) -> {
-            elements.add(new Message(typeMessage.getText().toString(), true));
-            aListAdapter.notifyDataSetChanged();
-            typeMessage.setText("");
-        });
+            //Create the new table:
+            onCreate(db);
+        }
 
-        receive.setOnClickListener((v) -> {
-            elements.add(new Message(typeMessage.getText().toString(), false));
-            aListAdapter.notifyDataSetChanged();
-            typeMessage.setText("");
-        });
     }
+
+//    private void getAll() {
+//        MyOpener dbHelper = new MyOpener(this);
+//        SQLiteDatabase db = dbHelper.getWritableDatabase();
+//
+//        String columns[] = {MyOpener.COL_ID, MyOpener.COL_MESSAGE, MyOpener.COL_SEND};
+//        Cursor c = db.query(false, MyOpener.TABLE_NAME, columns, null, null, null, null, null, null);
+//
+//        int msgColIndex = c.getColumnIndex(MyOpener.COL_MESSAGE);
+//        int sendColIndex = c.getColumnIndex(MyOpener.COL_SEND);
+//        int idColIndex = c.getColumnIndex(MyOpener.COL_ID);
+//
+//        //iterate over the results, return true if there is a next item:
+//        while (c.moveToNext()) {
+//            String name = c.getString(msgColIndex);
+//            boolean email = c.getInt(sendColIndex) == 1;
+//            long id = c.getLong(idColIndex);
+//
+//            //add the new Contact to the array list:
+//            elements.add(new Message(name, email, id));
+//        }
+//    }
+
+
 }
